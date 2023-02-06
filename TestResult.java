a48bb3962aa46be4665b7e651c185e36ee3e58ff
PRATYA PALEE <pratya.palee@gmail.com>
2023-02-06 14:24:57 +0700

    Testresult

---
+++ TestResult.java
@@ -0,0 +1,37 @@
+import java.beans.Transient;
+import static org.junit.Assert.assertEquals;
+import org.junit.*;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestResult {
+
+    public class assertEquals {
+        QuizOneJunit test = new QuizOneJunit();
+
+        int ans1 = test.square(10);
+        int ans2 = test.square(20);
+
+        @Before
+        public void setUp() {
+            System.out.println("@Before setUp");
+        }
+
+        @After
+        public void yearDown() {
+            System.out.println("@After tearDown");
+        }
+
+        @Test
+        public void testEqal() {
+            assertEquals(ans1, ans2);
+        }
+    }
+    public class AssertTrue {
+        QuizOneJunit test = new QuizOneJunit();
+        
+        private int val1 = 5;
+        private int val2 =6;
+    }
+}
